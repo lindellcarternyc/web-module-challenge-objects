0a56c9b0893896d08ac8a8815c09e75043829980
lindellcarternyc <lindellcarternyc@gmail.com>
2021-01-20 10:18:09 -0500

    solve task 7

--- index.js
+++ index.js
@@ -137,8 +137,9 @@ Use the getLastReview function below to do the following:
 */
 
 
-function getLastReview(/*Your code here*/) {
-  /*Your code here*/
+function getLastReview(reviewsArray) {
+  const { name, rating, feedback } = reviewsArray[reviewsArray.length - 1]
+  return `${name} gave the restaurant a ${rating} star review, and their feedback was: ${feedback}`
 } 
 
 
