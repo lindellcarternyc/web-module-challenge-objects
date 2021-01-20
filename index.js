429c6b498f5e5b4f00fb41725bccce885af4d39a
lindellcarternyc <lindellcarternyc@gmail.com>
2021-01-20 10:15:37 -0500

    complete task 6

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ const reviews = [
 Using the reviews array above:
   1. log only Julius' feedback to the console - no function needed 
 */
-
+console.log(reviews[5].feedback)
 
 
 
@@ -88,7 +88,12 @@ Using the reviews array above do the following: (no function needed)
   1. Following the same format (name, rating, feedback), add a new fictitious review object to the reviews array
   2. log the whole array to the console, make sure the new review is inside of it   
 */
-
+reviews.push({
+  name: 'Lindell',
+  rating: 5,
+  feedback: 'This is more feedback'
+})
+console.log(reviews)
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -96,8 +101,8 @@ Reyna's feedback is missing! Use what you know to do the following: (no function
   1. Add this feedback to Reyna's rating - "this place is chill with really cool people, great for getting work done on weekdays"
   2. log the reviews array to the console to check your work
 */
-
-
+reviews[8].feedback = "this place is chill with really cool people, great for getting work done on weekdays"
+console.log(reviews)
 
 
 
@@ -112,8 +117,9 @@ Use the getReviewByIndex function below to do the following:
 */
 
 
-function getReviewByIndex(/*Your code here*/) {
-  /*Your code here*/
+function getReviewByIndex(reviewsArray, index) {
+  const { name, rating, feedback } = reviewsArray[index]
+  return `${name} gave the restaurant a ${rating} star review, and their feedback was: ${feedback}`
 }
 
 
